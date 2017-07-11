680b5b4fdd5c004e0cf1945c7072f1192999e32a
QuantumT95 <tran.charlie@ymail.com>
2017-07-11 18:22:05 -0500

    final

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,42 +1,107 @@
-  var targetNumber = 53;
-  $("#number-to-guess").text(targetNumber);
-  var counter = 0;
-  // Now for the hard part. Creating multiple crystals each with their own unique number value.
-  // We begin by expanding our array to include four options.
-  var numberOptions = [10, 5, 3, 7];
-  // Next we create a for loop to create crystals for every numberOption.
-  for (var i = 0; i < numberOptions.length; i++) {
-    // For each iteration, we will create an imageCrystal
-    var imageCrystal = $("<img>");
-    // First each crystal will be given the class ".crystal-image".
-    // This will allow the CSS to take effect.
-    imageCrystal.addClass("crystal-image");
-    // Each imageCrystal will be given a src link to the crystal image
-    imageCrystal.attr("src", "http://cdn.playbuzz.com/cdn/35910209-2844-45c0-b099-f4d82878d54f/00261fda-4062-4096-81fd-8cf96b9034e8.jpg");
-    // Each imageCrystal will be given a data attribute called data-crystalValue.
-    // This data attribute will be set equal to the array value.
-    imageCrystal.attr("data-crystalvalue", numberOptions[i]);
-    // Lastly, each crystal image (with all it classes and attributes) will get added to the page.
-    $("#crystals").append(imageCrystal);
+//Global Variables------------------------------------- ----------
+
+var crys1 = Math.floor(Math.random() * (12 - 1) + 1);
+var crys2 = Math.floor(Math.random() * (12 - 1) + 1);
+var crys3 = Math.floor(Math.random() * (12 - 1) + 1);
+var crys4 = Math.floor(Math.random() * (12 - 1) + 1);
+
+
+//Counter
+var win = 0;
+var loss = 0;
+var finalScore;
+        
+
+//Functions------------------------------------------------------
+  function reset()
+  {
+    //reset
+     randNum = Math.floor(Math.random() * (120 - 19) + 19);
+     console.log('randomNumber =  ' + randNum);
+     crys1 = Math.floor(Math.random() * (12 - 1) + 1);
+     crys2 = Math.floor(Math.random() * (12 - 1) + 1);
+     crys3 = Math.floor(Math.random() * (12 - 1) + 1);
+     crys4 = Math.floor(Math.random() * (12 - 1) + 1)
+     finalScore = 0;
+
+     //HTML
+     $('.ranNumBox').html(randNum);
+     $('.scoreDisplayChild').html(finalScore);
+     start();
   }
-  // This time, our click event applies to every single crystal on the page. Not just one.
-  $(".crystal-image").on("click", function() {
-    // Determining the crystal's value requires us to extract the value from the data attribute.
-    // Using the $(this) keyword specifies that we should be extracting the crystal value of the clicked crystal.
-    // Using the .attr("data-crystalvalue") allows us to grab the value out of the "data-crystalvalue" attribute.
-    // Since attributes on HTML elements are strings, we must convert it to an integer before adding to the counter
-    
-    var crystalValue = ($(this).attr("data-crystalvalue"));
-    crystalValue = parseInt(crystalValue);
-    // We then add the crystalValue to the user's "counter" which is a global variable.
-    // Every click, from every crystal adds to the global counter.
-    counter += crystalValue;
-    // All of the same game win-lose logic applies. So the rest remains unchanged.
-    alert("New score: " + counter);
-    if (counter === targetNumber) {
-      alert("You win!");
-    }
-    else if (counter >= targetNumber) {
-      alert("You lose!!");
-    }
-  });
+
+  function start()
+  {
+     //reset
+     randNum = Math.floor(Math.random() * (120 - 19) + 19);
+     console.log('randomNumber =  ' + randNum);
+     crys1 = Math.floor(Math.random() * (12 - 1) + 1);
+     crys2 = Math.floor(Math.random() * (12 - 1) + 1);
+     crys3 = Math.floor(Math.random() * (12 - 1) + 1);
+     crys4 = Math.floor(Math.random() * (12 - 1) + 1)
+     finalScore = 0;
+
+     //HTML
+     $('.ranNumBox').html(randNum);
+     $('.scoreDisplayChild').html(finalScore);
+
+
+
+    //This gives each crystal a random value
+    $('.crystal1').attr("value", crys1);
+      var test = $('.crystal1').attr("value");
+      //Test / Debug
+      console.log('crystal 1   '+test);
+
+    $('.crystal2').attr("value", crys2);
+    var test = $('.crystal2').attr("value");
+    //Test / Debug
+    console.log('crystal 2   '+test);
+
+    $('.crystal3').attr("value", crys3);
+    var test = $('.crystal3').attr("value");
+    //Test / Debug
+    console.log('crystal 3   '+test);
+
+    $('.crystal4').attr("value", crys4);
+    var test = $('.crystal4').attr("value");
+    //Test / Debug
+    console.log('crystal 4   '+test);
+
+    //When clicked saves to Final score
+      //added the .off() because it will repeat the click twice without it and save the last number that
+      // was clicked and push it to the total score during the next game.
+    $('.box').off().on('click', function()
+    {
+      var test1 = $(this).attr('value');
+      //Adds to final score when clicked
+      finalScore = parseInt(finalScore) + parseInt(test1);
+      console.log('Crystal*  ' +  test1);
+      console.log('finalScore***  ' + finalScore);
+      $('.scoreDisplayChild').html(finalScore);
+
+      if(randNum === finalScore)
+      {
+        alert('win');
+        win++;
+        $('.win').html(win);
+        $('.scoreDisplayChild').html(0);
+        reset();
+        
+      }
+      else if (finalScore > randNum)
+      {
+        alert('loser');
+        loss++;
+        $('.loss').html(loss);
+        $('.scoreDisplayChild').html(0);
+        reset();
+      }
+
+    }); 
+
+
+}
+
+//Main------------------------------------
+start();
